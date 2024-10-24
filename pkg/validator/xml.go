7b6f2b6d2aafb416f264fd7c83a43c87aab39127
timwehrle <wetigg@gmail.com>
2024-10-24 10:49:15 +0200

    feat: start adding basic schema validation

--- pkg/validator/xml.go
+++ pkg/validator/xml.go
@@ -2,6 +2,11 @@ package validator
 
 import (
 	"encoding/xml"
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/lestrrat-go/libxml2"
 )
 
 type XMLValidator struct{}
@@ -14,5 +19,36 @@ func (XMLValidator) Validate(b []byte) (bool, error) {
 	if err != nil {
 		return false, err
 	}
+
+	doc, err := libxml2.ParseString(string(b))
+	if err != nil {
+		return false, err
+	}
+	defer doc.Free()
+
+	schemaLocation, err := extractSchemaLocation(string(b))
+	if err != nil {
+		return false, err
+	}
+
+	fmt.Println(schemaLocation)
+
 	return true, nil
+}
+
+func extractSchemaLocation(xmlContent string) (string, error) {
+	schemaLocationRegex := regexp.MustCompile(`xsi:schemaLocation\s*=\s*"([^"]+)"`)
+	noNamespaceSchemaLocationRegex := regexp.MustCompile(`xsi:noNamespaceSchemaLocation\s*=\s*"([^"]+)"`)
+
+	matches := schemaLocationRegex.FindStringSubmatch(xmlContent)
+	if len(matches) > 1 {
+		return matches[1], nil
+	}
+
+	noNsMatches := noNamespaceSchemaLocationRegex.FindStringSubmatch(xmlContent)
+	if len(noNsMatches) > 1 {
+		return noNsMatches[1], nil
+	}
+
+	return "", errors.New("no schema location found")
 }
